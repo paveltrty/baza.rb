6102965c57f7aef4028611453a27f64f7d465b50
Yegor Bugayenko <yegor256@gmail.com>
2025-06-03 08:03:26 +0300

    #75: Refactor code formatting for readability.

--- test/test_baza-rb.rb
+++ test/test_baza-rb.rb
@@ -571,8 +571,10 @@ class TestBazaRb < Minitest::Test
       .to_return(status: 0)
     error =
       assert_raises(BazaRb::ServerFailure) do
-        fake_baza.send(:checked,
-                       Typhoeus.get('https://example.org:443/test', headers: { 'X-Zerocracy-Token' => '000' }))
+        fake_baza.send(
+          :checked,
+          Typhoeus.get('https://example.org:443/test', headers: { 'X-Zerocracy-Token' => '000' })
+        )
       end
     assert_includes(error.message, 'Invalid response code #0')
     assert_includes(error.message, 'most likely a connection failure')
